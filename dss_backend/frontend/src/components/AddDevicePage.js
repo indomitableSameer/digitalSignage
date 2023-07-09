c566b44d4ff5a60dbfa95d079d4fd15a74d25ad8
Sameer Soni <indomitablesameer@gmail.com>
2023-07-09 20:31:47 +0200

    frontend

---
+++ dss_backend/frontend/src/components/AddDevicePage.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class AddDevicePage extends Component{
+    constructor(props) {
+        super(props);
+      }
+
+      render() {
+        return <p>Add Device page</p>;
+      }
+}
