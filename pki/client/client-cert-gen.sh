87fe9c0aef1edd73c2344969a6c6c7d00ed4671a
Sameer Soni <indomitablesameer@gmail.com>
2023-07-14 12:13:44 +0200

    setting up ca, server certificates, device certificate generation

---
+++ pki/client/client-cert-gen.sh
@@ -0,0 +1,10 @@
+rm device-ext.cnf 
+rm device-cert.pem
+
+echo "subjectAltName=DNS:*.device.com,DNS:localhost,IP:0.0.0.0" > device-ext.cnf
+echo "keyUsage=digitalSignature,keyEncipherment,nonRepudiation,dataEncipherment" >> device-ext.cnf
+echo "extendedKeyUsage=clientAuth" >> device-ext.cnf
+openssl x509 -req -in device.csr -CA ../ca/ca-cert.pem -CAkey ../ca/ca-key.pem -CAcreateserial -out device-cert.pem -extfile device-ext.cnf
+
+echo "device's signed certificate"
+openssl x509 -in device-cert.pem -noout -text
---
+++ pki/pki-gen_ecdsa.sh
@@ -0,0 +1,36 @@
+rm *.pem
+rm *.srl
+rm *.cnf
+rm *.csr
+rm *.cert
+
+sleep 5
+
+# 1. Generate CA's private key and self-signed certificate
+openssl ecparam -name prime256v1 -genkey -noout -out ca-key.pem
+openssl ec -in ca-key.pem -pubout -out ca-public-key.pem
+openssl req -new -x509 -key ca-key.pem -out ca-cert.pem -subj "/C=DE/ST=Hessen/L=FRA/O=FRA-UAS/OU=FB2/CN=*.test/emailAddress=test@gmail.com"
+
+echo "CA's self-signed certificate"
+openssl x509 -in ca-cert.pem -noout -text
+
+# 2. Generate web server's private key and certificate signing request (CSR)
+openssl ecparam -name prime256v1 -genkey -noout -out server-key.pem
+openssl ec -in server-key.pem -pubout -out server-public-key.pem
+openssl req -key server-key.pem -new -out server.csr -subj "/C=DE/ST=Hessen/L=FRA/O=FRA-UAS/OU=FB2/CN=*.tls/emailAddress=tls@gmail.com"
+
+echo "subjectAltName=DNS:*.tls,DNS:localhost,IP:0.0.0.0" > server-ext.cnf
+openssl x509 -req -in server.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
+
+echo "Server's signed certificate"
+openssl x509 -in server-cert.pem -noout -text
+
+
+# Remember that when we develop on localhost, It’s important to add the IP:0.0.0.0 as an Subject Alternative Name (SAN) extension to the certificate.
+#echo "subjectAltName=DNS:*.client.com,IP:0.0.0.0" > client-ext.cnf
+
+# 5. Use CA's private key to sign client's CSR and get back the signed certificate
+#openssl x509 -req -in client-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -extfile client-ext.cnf
+
+#echo "Client's signed certificate"
+#openssl x509 -in client-cert.pem -noout -text
