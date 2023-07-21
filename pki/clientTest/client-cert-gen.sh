8f241382dc50a41c1876ccfa4a521b7827c50c3e
Sameer Soni <indomitablesameer@gmail.com>
2023-07-21 02:49:26 +0200

    adding test client certs

---
+++ pki/clientTest/client-cert-gen.sh
@@ -0,0 +1,18 @@
+rm client-key.pem
+rm client-public-key.pem
+rm client.csr
+rm client-ext.cnf 
+rm client-cert.pem
+
+# 2. Generate web client's private key and certificate signing request (CSR)
+openssl ecparam -name prime256v1 -genkey -noout -out client-key.pem
+openssl ec -in client-key.pem -pubout -out client-public-key.pem
+openssl req -key client-key.pem -new -out client.csr -subj "/C=DE/ST=Hessen/L=FRA/O=FRA-UAS/OU=FB2/CN=b8:27:eb:e8:40:6f/emailAddress=tls@gmail.com"
+
+echo "subjectAltName=DNS:*.client.com,IP:0.0.0.0" > client-ext.cnf
+echo "keyUsage=digitalSignature,keyEncipherment,nonRepudiation,dataEncipherment" >> client-ext.cnf
+echo "extendedKeyUsage=clientAuth" >> client-ext.cnf
+openssl x509 -req -in client.csr -CA ../ca/ca-cert.pem -CAkey ../ca/ca-key.pem -CAcreateserial -out client-cert.pem -extfile client-ext.cnf
+
+echo "client's signed certificate"
+openssl x509 -in client-cert.pem -noout -text
