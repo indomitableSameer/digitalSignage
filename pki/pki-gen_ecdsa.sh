8a2da4af7eb3031e7d174f2b29384515577f3dbb
Sameer Soni <indomitablesameer@gmail.com>
2023-07-21 03:35:36 +0200

    certificates added

--- pki/pki-gen_ecdsa.sh
+++ pki/pki-gen_ecdsa.sh
@@ -9,21 +9,21 @@ sleep 5
 # 1. Generate CA's private key and self-signed certificate
 openssl ecparam -name prime256v1 -genkey -noout -out ca-key.pem
 openssl ec -in ca-key.pem -pubout -out ca-public-key.pem
-openssl req -new -x509 -key ca-key.pem -out ca-cert.pem -subj "/C=DE/ST=Hessen/L=FRA/O=FRA-UAS/OU=FB2/CN=*.test/emailAddress=test@gmail.com"
+openssl req -new -x509 -key ca-key.pem -out ca-cert.pem -subj "/C=DE/ST=Hessen/L=FRA/O=FRA-UAS/OU=FB2/CN=TEST_CA/emailAddress=test@gmail.com"
 
 echo "CA's self-signed certificate"
 openssl x509 -in ca-cert.pem -noout -text
 
 # 2. Generate web server's private key and certificate signing request (CSR)
-openssl ecparam -name prime256v1 -genkey -noout -out server-key.pem
-openssl ec -in server-key.pem -pubout -out server-public-key.pem
-openssl req -key server-key.pem -new -out server.csr -subj "/C=DE/ST=Hessen/L=FRA/O=FRA-UAS/OU=FB2/CN=*.tls/emailAddress=tls@gmail.com"
+#openssl ecparam -name prime256v1 -genkey -noout -out server-key.pem
+#openssl ec -in server-key.pem -pubout -out server-public-key.pem
+#openssl req -key server-key.pem -new -out server.csr -subj "/C=DE/ST=Hessen/L=FRA/O=FRA-UAS/OU=FB2/CN=TEST_CA/emailAddress=tls@gmail.com"
 
-echo "subjectAltName=DNS:*.tls,DNS:localhost,IP:0.0.0.0" > server-ext.cnf
-openssl x509 -req -in server.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
+#echo "subjectAltName=DNS:TEST_CA,DNS:localhost,IP:0.0.0.0" > server-ext.cnf
+#openssl x509 -req -in server.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
 
-echo "Server's signed certificate"
-openssl x509 -in server-cert.pem -noout -text
+#echo "Server's signed certificate"
+#openssl x509 -in server-cert.pem -noout -text
 
 
 # Remember that when we develop on localhost, It’s important to add the IP:0.0.0.0 as an Subject Alternative Name (SAN) extension to the certificate.
