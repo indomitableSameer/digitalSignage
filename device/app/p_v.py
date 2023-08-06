9356de4438b5b9c280a67c440b8a597405d95a24
sameer soni <indomitablesameer@gmail.com>
2023-08-06 22:14:07 +0100

    code add

--- device/app/p_v.py
+++ device/app/p_v.py
@@ -23,15 +23,15 @@ app_log.addHandler(my_handler)
 
 async def main():
 	app_log.info('dss player stating..')
-	player_thread = vlcplayer(app_log, "./media/vv.mp4")
-	app_log.info('starting vlc player thread..')
+	#player_thread = vlcplayer(app_log, "./media/vv.mp4")
+	#app_log.info('starting vlc player thread..')
 	#player_thread.start()
 	#app_log.debug("other work")
 	#player_thread.join(timeout=None)
 	c = channel(app_log, "device.dss.com:4001", "./pki/client-cert.pem", "./pki/client-key.pem", "./pki/ca-cert.pem")
 	await registerDevice(c.get_channel())
 	#registerDevice(c.get_channel())
-	time.sleep(500)
+	#time.sleep(500)
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
