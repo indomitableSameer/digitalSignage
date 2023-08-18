7ab2a74189fe0832881c93b4b46607a3ffa2da5c
Sameer Soni <indomitablesameer@gmail.com>
2023-08-19 01:48:22 +0200

    updating cert to 365 days validatiy

--- pki/device-server/device-server-cert-gen.sh
+++ pki/device-server/device-server-cert-gen.sh
@@ -7,12 +7,12 @@ rm device-server-cert.pem
 # 2. Generate web server's private key and certificate signing request (CSR)
 openssl ecparam -name prime256v1 -genkey -noout -out device-server-key.pem
 openssl ec -in device-server-key.pem -pubout -out device-server-public-key.pem
-openssl req -key device-server-key.pem -new -out device-server.csr -subj "/C=DE/ST=Hessen/L=FRA/O=FRA-UAS/OU=FB2/CN=device.dss.com/emailAddress=tls@gmail.com"
+openssl req -key device-server-key.pem -new -out device-server.csr -subj "/C=DE/ST=Hessen/L=FRA/O=FRA-UAS/OU=FB2/CN=device.dss.com/emailAddress=tls@gmail.com" 
 
 echo "subjectAltName=DNS:device.dss.com,DNS:localhost,IP:0.0.0.0" > device-server-ext.cnf
 echo "keyUsage=digitalSignature,keyEncipherment,nonRepudiation,dataEncipherment" >> device-server-ext.cnf
 echo "extendedKeyUsage=serverAuth,clientAuth" >> device-server-ext.cnf
-openssl x509 -req -in device-server.csr -CA ../ca/ca-cert.pem -CAkey ../ca/ca-key.pem -CAcreateserial -out device-server-cert.pem -extfile device-server-ext.cnf
+openssl x509 -req -in device-server.csr -CA ../ca/ca-cert.pem -CAkey ../ca/ca-key.pem -CAcreateserial -out device-server-cert.pem -extfile device-server-ext.cnf -days 365
 
 echo "Server's signed certificate"
 openssl x509 -in device-server-cert.pem -noout -text
