0db306198fb1c2e3b244f991f0180de2935e1965
indomitablesameer <indomitablesameer@gmail.com>
2023-08-19 02:53:31 +0200

    cleanup tasks

---
+++ device/app/old/registerDevice.py
@@ -0,0 +1,7 @@
+import deviceRegistration_pb2
+import digitalSignage.device.app.old.deviceRegistration_pb2_grpc as deviceRegistration_pb2_grpc
+
+async def registerDevice(grpcChannel) -> None:
+    stub = deviceRegistration_pb2_grpc.DeviceRegistrationStub(channel=grpcChannel)
+    response = stub.RegisterDevice(deviceRegistration_pb2.DeviceRegistrationRequest(device_mac_add="test1", device_unique_id="test1", fw_version="test1", os_version="test1"))
+    print(response)
