92a37a86d4509b038a1b26e9f7702ed141f97d46
Sameer Soni <indomitablesameer@gmail.com>
2023-08-25 17:13:46 +0200

    adding commit info db table

--- backend_servers/d_server/main.go
+++ backend_servers/d_server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/google/uuid"
 	dbprovider "github.com/indomitableSameer/digitalSignage/backend_servers/dbProvider"
 	"github.com/indomitableSameer/digitalSignage/backend_servers/dbentities"
 	requesthandlers "github.com/indomitableSameer/digitalSignage/backend_servers/requestHandlers"
@@ -16,6 +17,10 @@ func main() {
 	fmt.Println("creating db..")
 	dbprovider.DBObj.AutoMigrate(&dbentities.DeviceList{})
 	dbprovider.DBObj.AutoMigrate(&dbentities.DeviceStatus{})
+	dbprovider.DBObj.AutoMigrate(&dbentities.ContentInfo{})
+
+	content := dbentities.ContentInfo{ContentId: uuid.New(), FileName: "test.mp4"}
+	dbprovider.DBObj.Create(&content)
 
 	multiplexer := mux.NewRouter()
 	multiplexer.HandleFunc("/status", requesthandlers.HandleStatusRequest).Methods(http.MethodPut)
--- temp/test/startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-service docker start;
-docker pull hello-world;
-docker run hello-world;
