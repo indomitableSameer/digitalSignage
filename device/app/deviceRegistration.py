a7e97ec8584764cd5e32fd3ed57c0b6ba6d8c936
indomitablesameer <indomitablesameer@gmail.com>
2023-09-21 22:25:29 +0200

    device registration test, working

--- device/app/deviceRegistration.py
+++ device/app/deviceRegistration.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import configparser
+import time
 import M2Crypto.SSL
 import M2Crypto.Engine
 import M2Crypto.X509
@@ -41,34 +42,38 @@ def write_config(config):
 
 
 def registerDevice(log: logging, connection: M2Crypto.httpslib.HTTPSConnection):
-    config = read_config()
+    try:
+        config = read_config()
 
-    if reg_state.status == True:
-        log.info("Device already registered.. checking other info")
-        if reg_state.port != 0 and len(reg_state.serverUrl) != 0:
-            log.info("info stored in init " + reg_state.serverUrl + ':' +
-                     str(reg_state.port) + " looks okay. Not sending registration request..")
+        if reg_state.status == True:
+            log.info("Device already registered.. checking other info")
+            if reg_state.port != 0 and len(reg_state.serverUrl) != 0:
+                log.info("info stored in init " + reg_state.serverUrl + ':' +
+                        str(reg_state.port) + " looks okay. Not sending registration request..")
+                return True
+
+        log.info("sending registration request..")
+        headers = {'Content-type': 'application/json'}
+        connection.connect()
+        connection.request("POST", "/registerDevice",
+                        json.dumps(DeviceRegisterRequest().__dict__), headers)
+        response = connection.getresponse()
+        print(response)
+        body = json.loads(response.read().decode())
+        connection.close()
+        log.info("closed connection! Going to entract and save info received..")
+        log.info("registration response -->" + json.dumps(body))
+        if int(body['RegistrationStatus']) == 1 or int(body['RegistrationStatus']) == 2:
+            reg_state.serverUrl = config['backend.service']['Url'] = body['ServiceUrl']
+            reg_state.port = config['backend.service']['Port'] = body['ServicePort']
+            reg_state.systemid = config['backend.service']['SystemId'] = body['UniqueSystemId']
+            config['DEFAULT']['registered'] = 'yes'
+            config['DEFAULT']['Timezone'] = body['Timezone']
+            write_config(config)
+            reg_state.status = True
             return True
-
-    log.info("sending registration request..")
-    headers = {'Content-type': 'application/json'}
-    connection.connect()
-    connection.request("POST", "/register",
-                       json.dumps(DeviceRegisterRequest().__dict__), headers)
-    response = connection.getresponse()
-    print(response)
-    body = json.loads(response.read().decode())
-    connection.close()
-    log.info("closed connection! Going to entract and save info received..")
-    log.info("registration response -->" + json.dumps(body))
-    if int(body['RegistrationStatus']) == 1 or int(body['RegistrationStatus']) == 2:
-        reg_state.serverUrl = config['backend.service']['Url'] = body['ServiceUrl']
-        reg_state.port = config['backend.service']['Port'] = body['ServicePort']
-        reg_state.systemid = config['backend.service']['SystemId'] = body['UniqueSystemId']
-        config['DEFAULT']['registered'] = 'yes'
-        config['DEFAULT']['Timezone'] = body['Timezone']
-        write_config(config)
-        reg_state.status = True
-        return True
-    else:
-        return False
+        else:
+            return False
+    except Exception as e:
+        log.error(e)
+        time.sleep(300)
