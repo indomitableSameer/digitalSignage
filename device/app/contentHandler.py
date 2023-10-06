3f310fca8eaaf99e35bd459a19659aeb23207a3c
Sameer Soni <indomitablesameer@gmail.com>
2023-10-06 17:47:52 +0200

    device changes

--- device/app/contentHandler.py
+++ device/app/contentHandler.py
@@ -1,14 +1,18 @@
-import json, os
+import json
+import os
 import logging
 from time import sleep
 import appdb as appdb
 import secure_conn as secure_conn
 from httpStatus import HttpStatus
 import globalVariables as gv
+import shutil
 
 FILE_PATH = "./media/content.mp4"
+TEMP_FILE_PATH = "./media/content.mp4"
 
-def getUpdatedContent(log:logging):
+
+def getUpdatedContent(log: logging):
     while True:
         try:
             log.info("waiting on content_event..")
@@ -19,34 +23,43 @@ def getUpdatedContent(log:logging):
                 contentinfo_details = appdb.getPlayScheduleFromDb()
                 if reg_details.reg_id != None:
                     log.info("sending content request request..")
-                    connection = secure_conn.getConnection(conn_details.service_url, int(conn_details.service_port)) 
+                    connection = secure_conn.getConnection(
+                        conn_details.service_url, int(conn_details.service_port))
                     connection.connect()
 
                     headers = {'Content-type': 'application/json'}
                     req = {"RegistrationId": reg_details.reg_id}
-                    connection.request("GET", "/getContent", json.dumps(req), headers) 
+                    connection.request("GET", "/getContent",
+                                       json.dumps(req), headers)
 
                     response = connection.getresponse()
-                    log.info("Status Update: server response code for content req -> " + str(response.status))
-                    #print(response.getheaders())
+                    log.info(
+                        "Status Update: server response code for content req -> " + str(response.status))
+                    # print(response.getheaders())
 
                     if response.status == HttpStatus.OK:
                         contentId = response.headers.get('Content-Id')
                         contentLen = response.headers.get('Content-Length')
-                        with open(FILE_PATH, 'wb') as out_file:
-                            #print("going to read response")
+                        with open(TEMP_FILE_PATH, 'wb') as out_file:
+                            # print("going to read response")
                             while chunk := response.read(200):
-                                #print(len(chunk))
-                                #shutil.copy(chunk, out_file)
+                                # print(len(chunk))
+                                # shutil.copy(chunk, out_file)
                                 out_file.write(chunk)
-                            #body = response.read()
-                            #out_file.write(body)
-                    #print("going to close channel")
-                    log.info("rec file size :" + str(os.path.getsize(FILE_PATH)))
+                            # body = response.read()
+                            # out_file.write(body)
+                    # print("going to close channel")
+                    log.info("rec file size :" +
+                             str(os.path.getsize(TEMP_FILE_PATH)))
                     log.info("header file size :" + str(contentLen))
-                    log.info("content id : "+ contentId)
+                    log.info("content id : " + contentId)
+                    if os.path.getsize(TEMP_FILE_PATH) == int(contentLen):
+                        log.info(
+                            "file downloaded successfully. moving from temp place to media location..")
+                        shutil.move(TEMP_FILE_PATH, FILE_PATH)
                     connection.close()
-                    appdb.InsertOrUpdateContentInfoInDb(contentinfo_details.id, contentId, "content.mp4", FILE_PATH, "mp4")
+                    appdb.InsertOrUpdateContentInfoInDb(
+                        contentinfo_details.id, contentId, "content.mp4", FILE_PATH, "mp4")
                 log.info("clearing content_event..")
                 gv.content_event.clear()
         except Exception as e:
--- device/app/vlcplayer.py
+++ device/app/vlcplayer.py
@@ -1,16 +1,18 @@
 # importing time and vlc
-import vlc, time
+import vlc
+import time
 import globalVariables as gv
 import os
+
 
 class vlcplayer():
     def __init__(self, logger, media_file):
         self.is_playing = False
         self.media_file = media_file
         self.logger = logger
-        #self.vlc_instance = vlc.Instance("--aout=adummy --embedded-video --file-logging --logfile=vlc-log.txt --verbose 3")
+        # self.vlc_instance = vlc.Instance("--aout=adummy --embedded-video --file-logging --logfile=vlc-log.txt --verbose 3")
         self.vlc_instance = vlc.Instance("--aout=adummy --embedded-video")
-        #self.vlc_instance.vlm_set_loop(media_file, True)
+        # self.vlc_instance.vlm_set_loop(media_file, True)
         self.player = self.vlc_instance.media_player_new()
         logger.info('vlc player init done.')
 
@@ -29,31 +31,29 @@ class vlcplayer():
         self.player.stop()
 
     def run(self):
-        isContentRequestRunning = False
         while True:
             try:
-                if self.is_playing != True and isContentRequestRunning != True and gv.schedule_active.wait() == True:
+                if self.is_playing != True and gv.schedule_active.wait() == True:
                     if self._checkFileExistance() == True:
-                        self._startplay() #uncomment to run actual
-                        self.logger.info('received schedule_active event set, start playing')
+                        self._startplay()  # uncomment to run actual
+                        self.logger.info(
+                            'received schedule_active event set, start playing')
                         self.is_playing = True
-                        isContentRequestRunning = False
                     else:
-                        self.logger.info('content file now found, setting content event..')
+                        self.logger.info(
+                            'content file now found, setting content event..')
                         gv.content_event.set()
-                        isContentRequestRunning = True
                 elif gv.schedule_active.is_set() != True:
                     self._stopplay()
-                    self.logger.info('received schedule_active event cleared, stoping playing..')
+                    self.logger.info(
+                        'received schedule_active event cleared, stoping playing..')
                     self.is_playing = False
             except Exception as e:
                 self.logger.info(e)
                 time.sleep(30)
-    
+
     def _checkFileExistance(self):
-        if os.path.exists(self.media_file) == True and os.path.getsize(self.media_file) != 0 :
+        if os.path.exists(self.media_file) == True and os.path.getsize(self.media_file) != 0:
             return True
         else:
             return False
-
-
